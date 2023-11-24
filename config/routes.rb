4b2b555df095ab65ff9a7c4852c36c9b1cc2db46
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2023-11-24 20:36:12 +0800

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  get '/' => 'users#index'
+  get '/' => 'articles#index'
   # root "posts#index"
 end
